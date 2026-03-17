577d1adefec91da4802a93b6f15ecbbce9ba9098
Ron Asnahon <asnahonron@gmail.com>
2026-03-17 22:33:04 +0800

    Create index.ts

---
+++ assembly/index.ts
@@ -0,0 +1,8 @@
+// assembly/index.ts
+export fn calculate_simple_checksum(data: Uint8Array): u32 {
+  let hash: u32 = 0;
+  for (let i = 0; i < data.length; i++) {
+    hash = (hash << 5) - hash + data[i];
+  }
+  return hash;
+}
